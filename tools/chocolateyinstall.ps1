3fd914a8cb762e5f2fc4ad96b037a769790c08d2
赵安家 <anjia0532@gmail.com>
2021-05-16 14:17:47 +0800

    first_commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,49 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$softwareName = 'uTools*'
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+
+if ($key.Count -gt 0) {
+    Invoke-Expression -Command $PSScriptRoot\chocolateyUninstall.ps1
+}
+
+$url = 'https://res.u-tools.cn/currentversion/uTools-1.3.5.exe'
+$sha256sum = '6ffc7a649a3a432476d8f73e8235eb92984e50fb6ac3058759c301de7cd13f1a'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+if ([System.Environment]::Is64BitOperatingSystem) {
+    $programFiles = $env:ProgramFiles
+}
+else {
+    $programFiles = ${env:ProgramFiles(x86)}
+}
+$installDir = "$programFiles\utools"
+
+$pp = Get-PackageParameters
+if ($pp.InstallDir) {
+    $installDir = $pp.InstallDir
+}
+
+$silentArgs = "/S /D=$installDir"
+
+New-Item -ItemType Directory -Force -Path $installDir
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    url            = $url
+    url64bit       = $url
+
+    softwareName   = $softwareName
+
+    checksum       = $sha256sum
+    checksumType   = 'sha256'
+    checksum64     = $sha256sum
+    checksumType64 = 'sha256'
+
+    silentArgs     = $silentArgs
+    validExitCodes = @(0, 1641, 3010)
+}
+
+Install-ChocolateyPackage @packageArgs
